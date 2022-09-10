8a9b671c714494eb5e9c8cb254a59acf9dc3c8ae
Rohan Allison <rohan.allison@cbre.com>
2022-09-10 16:24:11 -0500

    Working on print outs

--- main.go
+++ main.go
@@ -6,6 +6,36 @@ import (
 )
 
 func main() {
+	name := "John"
+	fmt.Println("name is:", name)
+
+	aNum := 1
+	fmt.Println(aNum)
+
+	var anotherNum int = 2
+	println(anotherNum)
+
+	nameN := "Noah"
+	println(nameN)
+
+	var name2 string
+	name2 = "christian"
+	println(name2)
+
+	// Take some input and hash it
+	var input string
+	print("Please enter a word to hash ")
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Println("My error:", err)
+		return
+	}
+
+	// Hash the input
+	sum := sha256.Sum256([]byte(input + "sgyuff$$$!!!~)"))
+	fmt.Printf("%x\n", sum)
+	// fmt.Printf("Hash: %x\n", sum)
+
 	/*
 		var a int
 		var b int
@@ -18,16 +48,4 @@ func main() {
 		f = 21
 	*/
 	// fmt.Println("The sum is:", a+b+c+f)
-
-	var input string
-	_, err := fmt.Scanln(&input)
-	if err != nil {
-		fmt.Println("My error:", err)
-		return
-	}
-
-	// Hash the input
-	sum := sha256.Sum256([]byte(input + "sgyuff$$$!!!~)"))
-	fmt.Printf("%x\n", sum)
-	// fmt.Printf("Hash: %x\n", sum)
 }
